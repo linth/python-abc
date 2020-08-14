983b3d8beeb65146682612b7d0cb16181d4b04d0
george_lin <george_lin@goglobal.com.tw>
2020-08-14 15:20:26 +0800

    add utils for obj2json and timestamp.

---
+++ utils/json_utils.py
@@ -0,0 +1,18 @@
+"""
+References:
+    - http://tdongsi.github.io/blog/2016/05/21/convert-python-objects-to-json/
+"""
+import json
+
+
+def obj_to_json(given: object) -> str:
+    """
+    convert to json from object.
+    :param given:
+    :return:
+    """
+    try:
+        return json.dumps(given, default=vars, indent=4)
+    except Exception as e:
+        print(f'error on obj_to_json: {e}')
+        return ''
