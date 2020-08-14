42653682c4f75cacc5e3dd050c0d8f743d78f00c
george_lin <george_lin@goglobal.com.tw>
2020-08-14 15:19:18 +0800

    add new attr and to do list.

--- exception/basic_exception.py
+++ exception/basic_exception.py
@@ -1,12 +1,24 @@
+from mixin.saveExceptionMixin import SaveExceptionMixin
+
+
+def save():
+    pass
 
 
 class IeBaseException(Exception):
     """ the basic exception. """
     def __init__(self, class_name, function_name, msg=None, code=None):
+        # TODO: should add the information as follows:
+        #  1) exception error.
+        #  2) django model.
+        #  3) access these information of exception with Mixin class.
+
         self.class_name = class_name
         self.function_name = function_name
         self.message = msg
         self.status_code = code
+        # self.error_msg = None
+        # self.model = None
 
 
 class GeneralException(IeBaseException):
