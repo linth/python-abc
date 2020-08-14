a5e3dae4e3a255757a6c7a05a20121b85e6b98d8
george_lin <george_lin@goglobal.com.tw>
2020-08-14 15:18:05 +0800

    define rx-action ABC class.

--- chain_operations.py
+++ chain_operations.py
@@ -1,5 +1,6 @@
 from abc import ABC
 from typing import Generic, TypeVar
+from .exception.basic_exception import IeBaseException
 
 
 T = TypeVar("T")
@@ -16,6 +17,27 @@ class IeConversionDelegate(ABC):
         raise NotImplementedError('do_convert() must be overridden.')
 
 
+class IeAbsConverter(Generic[T], IeConversionDelegate, ABC):
+    def do_convert(self, item: T) -> T:
+        try:
+            return self.execute_conversion(item)
+        except Exception as e:
+            if isinstance(e, IeBaseException):
+                raise e
+            else:
+                raise IeBaseException(__class__.__name__,
+                                      'execute_conversion',
+                                      'Error on involve execute_conversion()!',)
+
+    def execute_conversion(self, item: T) -> T:
+        """
+        execute conversion function.
+        :param item:
+        :return:
+        """
+        raise NotImplementedError('execute_conversion() must be overridden.')
+
+
 class IeCallableDelegate(ABC):
     """ only have output. """
     def call(self) -> T:
@@ -24,6 +46,22 @@ class IeCallableDelegate(ABC):
         :return:
         """
         return NotImplementedError('call() must be overridden.')
+
+
+class IeAbsCallable(Generic[T], IeCallableDelegate, ABC):
+    def call(self) -> T:
+        try:
+            return self.execute_generation()
+        except Exception as e:
+            if isinstance(e, IeBaseException):
+                raise e
+            else:
+                raise IeBaseException(__class__.__name__,
+                                      'execute_generation',
+                                      'Error on involve execute_generation()!',)
+
+    def execute_generation(self) -> T:
+        raise NotImplementedError('execute_generation() must be overridden.')
 
 
 class IeFilterDelegate(ABC):
@@ -36,6 +74,21 @@ class IeFilterDelegate(ABC):
         """
         raise NotImplementedError('predicate() must be overridden.')
 
+
+class IeFilter(Generic[T], IeFilterDelegate, ABC):
+    def predicate(self, item: T) -> bool:
+        try:
+            return self
+        except Exception as e:
+            if isinstance(e, IeBaseException):
+                return e
+            else:
+                raise IeBaseException(__class__.__name__,
+                                      'execute_judgement',
+                                      'Error on involve execute_judgement()!',)
+
+    def execute_judgement(self, item: T) -> bool:
+        raise NotImplementedError('execute_judgement() must be overridden.')
 
 # TODO: think about the situation as follows:
 #  1) only have input.
@@ -65,3 +118,15 @@ class IeChain(Generic[T]):
 
     def on_complete(self) -> T:
         return self.ie_data_holder.kept_data
+
+
+class A(IeAbsConverter):
+    def execute_conversion(self, item: T) -> T:
+        print(f'{__class__.__name__}, execute_conversion')
+        item += 3
+        print(item)
+
+
+if __name__ == '__main__':
+    a = A()
+    a.do_convert(3)
